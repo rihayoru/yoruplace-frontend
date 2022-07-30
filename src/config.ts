2198c87de2f6c072b83c83cd3aeeb1d4d273b33d
Yoru <me@rlato.icu>
2022-07-30 13:48:15 +0900

    chore: typo

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export class config {
   static readonly API_URL = 'https://api.rlato.icu:3000'
-  static readonly DISCORD_CALLBACK_URI = 'https://your.rlato.icu/callback'
+  static readonly DISCORD_CALLBACK_URI = 'https://yoru.rlato.icu/callback'
 }
