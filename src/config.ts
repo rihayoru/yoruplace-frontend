a3ad8fba1927f744b716fa34e9f7a5f5311c34ee
Yoru <me@rlato.icu>
2022-07-31 01:42:16 +0900

    build: fix api url

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export class config {
   // static readonly API_URL = 'http://localhost:3000'
   // static readonly DISCORD_CALLBACK_URI = 'http://localhost:8080/callback'
   // static readonly KAKAO_CALLBACK_URI = 'http://localhost:8080/verify/callback'
-  static readonly API_URL = 'https://api.rlato.icu/'
+  static readonly API_URL = 'https://api.rlato.icu'
   static readonly DISCORD_CALLBACK_URI = 'https://yoru.rlato.icu/callback'
   static readonly KAKAO_CALLBACK_URI = 'https://yoru.rlato.icu/verify/callback'
 }
