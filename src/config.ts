6b9dc65a46cbf94e7947612d0c6c80cfe3e29082
Yoru <me@rlato.icu>
2022-08-02 21:14:41 +0900

    build: chang config to yoru.icu

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ export class config {
   // static readonly DISCORD_CALLBACK_URI = 'http://localhost:8080/callback'
   // static readonly KAKAO_CALLBACK_URI = 'http://localhost:8080/verify/callback'
   static readonly API_URL = 'https://api.rlato.icu'
-  static readonly DISCORD_CALLBACK_URI = 'https://yoru.rlato.icu/callback'
-  static readonly KAKAO_CALLBACK_URI = 'https://yoru.rlato.icu/verify/callback'
+  static readonly DISCORD_CALLBACK_URI = 'https://yoru.icu/callback'
+  static readonly KAKAO_CALLBACK_URI = 'https://yoru.icu/verify/callback'
 }
